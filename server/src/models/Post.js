cb6060f7a385648e79920c57693cc9e5f2170af7
baillyjean8-collab <baillyjean8@gmail.com>
2026-07-17 18:29:05 +0200

    Update Post.js

--- server/src/models/Post.js
+++ server/src/models/Post.js
@@ -1,6 +1,7 @@
 'use strict';
 const mongoose = require('mongoose');
 const commentSchema = new mongoose.Schema({
+parentId: { type: mongoose.Schema.Types.ObjectId, default: null },
   userId:    { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   text:      { type: String, required: true, maxlength: 500 },
   createdAt: { type: Date, default: Date.now },
