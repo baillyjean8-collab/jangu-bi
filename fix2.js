3e45c20ecf866f8af713ee8e7c61814bb847ff3f
baillyjean8-collab <baillyjean8@gmail.com>
2026-07-18 19:22:56 +0200

    Delete fix2.js

--- fix2.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = 'C:\\Users\\hp\\Desktop\\jangu bi\\jangu-bi\\client\\src\\pages\\parishes\\index.jsx';
-let content = fs.readFileSync(path, 'utf8');
-const lines = content.split('\n');
-// Afficher ligne 144-146
-console.log('Ligne 144:', lines[143]);
-console.log('Ligne 145:', lines[144]);
-console.log('Ligne 146:', lines[145]);
