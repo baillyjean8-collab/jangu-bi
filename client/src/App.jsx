5b2f71776faa1d8f94c7f0e15e39db9540566aff
baillyjean8-collab <baillyjean8@gmail.com>
2026-07-20 12:50:16 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from './pages/home';
 import CreatePostPage from './pages/create-post';
 import CreateStoryPage from './pages/create-story';
 import AnnouncementsPage from './pages/annonces';
+import BibliothequePage from './pages/bibliotheque';
 import { ParishesPage } from './pages/parishes';
 import ParishDetail from './pages/parishes/ParishDetail';
 import CatechesePage from './pages/catechese';
@@ -65,6 +66,7 @@ export default function App() {
             <Route path="/parishes/:id" element={<ProtectedRoute><ParishDetail /></ProtectedRoute>} />
             <Route path="/announcements" element={<ProtectedRoute><AnnouncementsPage /></ProtectedRoute>} />
             <Route path="/annonces"     element={<ProtectedRoute><AnnouncementsPage /></ProtectedRoute>} />
+            <Route path="/bibliotheque" element={<ProtectedRoute><BibliothequePage /></ProtectedRoute>} />
             <Route path="/live"         element={<ProtectedRoute><LiveListPage /></ProtectedRoute>} />
             <Route path="/live/:id"     element={<ProtectedRoute><LiveScreen /></ProtectedRoute>} />
             <Route path="/catechese"    element={<ProtectedRoute><CatechesePage /></ProtectedRoute>} />
