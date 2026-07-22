48d44a06a7819b48aa3bd0cb51bebcfc6c1a5a3d
baillyjean8-collab <baillyjean8@gmail.com>
2026-07-22 14:58:59 +0200

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -189,6 +189,7 @@ app.use('/api/announcements', announcementRoutes);
 app.use('/api/bible', bibleRoutes);
 app.use('/api/groups', groupRoutes);
 app.use('/api/invitations', invitationRoutes);
+app.use('/api/_seed', require('./domains/admin/seed-route'));
 app.use('/api/parish-events', require('./domains/parish-events'));
 
 
