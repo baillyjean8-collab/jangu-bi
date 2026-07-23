d7d5d0a3e8b674e51a8219b4a014a2b63114d42a
baillyjean8-collab <baillyjean8@gmail.com>
2026-07-23 10:41:53 +0200

    Update index.jsx

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -451,7 +451,7 @@ setReplyingTo(null);
   function grilleImages(post, i) {
 if (post.video) {
 return (
-<video src={post.video} controls playsInline preload="metadata" muted={false} style={{ width: '100%', display: 'block', maxHeight: 600, objectFit: 'contain', background: '#000' }} onLoadedData={function(e) { e.target.currentTime = 0.1; }} />
+<VideoPublication src={post.video} sonActifGlobal={sonActifGlobal} setSonActifGlobal={setSonActifGlobal} />
 );
 }
 const imgs = post.images && post.images.length ? post.images : (post.image ? [post.image] : []);
