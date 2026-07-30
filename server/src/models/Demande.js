955096359f0c9f982b0c49539dbdac4a2b089b7b
baillyjean8-collab <baillyjean8@gmail.com>
2026-07-30 11:02:39 +0200

    Create Demande.js

---
+++ server/src/models/Demande.js
@@ -0,0 +1,31 @@
+'use strict';
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const demandeSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true, index: true },
+  type: {
+    type: String,
+    required: true,
+    enum: ['messe', 'bapteme', 'mariage', 'confirm', 'communion', 'sepulture', 'parrain', 'transfert', 'rdv'],
+  },
+  titre: { type: String, required: true },
+  statut: { type: String, enum: ['en_attente', 'validee', 'rejetee'], default: 'en_attente' },
+  montant: { type: Number, default: 0 },
+  reference: { type: String, required: true, unique: true },
+  pourQui: { type: String, enum: ['moi', 'autre'], default: 'moi' },
+  nomBeneficiaire: { type: String, default: '' },
+  lienParente: { type: String, default: '' },
+  telephoneContact: { type: String, default: '' },
+  paroisseConcernee: { type: String, default: '' },
+  pretreOfficiant: { type: String, default: '' },
+  dateEvenement: { type: Date, default: null },
+  intentionMesse: { type: String, default: '' },
+  momentMesse: { type: String, default: '' },
+  rdvMotif: { type: String, default: '' },
+  rdvMessage: { type: String, default: '' },
+  dateTraitement: { type: Date, default: null },
+  noteAdmin: { type: String, default: '' },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Demande', demandeSchema);
